0e150859914700e0eeef740d16ece85caf9bfe4c
hasturseven <hasturseven@gmail.com>
2022-06-29 15:43:17 -0500

    ARREGLADO EL PROBLEMA QUE NO ME CORRIA EL ARCHIVO DE PANDAS

--- PANDASeriesaAndDataFrames.py
+++ PANDASeriesaAndDataFrames.py
@@ -3,3 +3,4 @@ import pandas as pd
 psg_players = pd.Series(['Navas', 'Mbappe', 'Neymar', 'Messie'],
                         index=[1, 7, 10, 30]
                         ) # le estoy dando el index a cada jugador osea ese sera su identificador
+print(psg_players)
